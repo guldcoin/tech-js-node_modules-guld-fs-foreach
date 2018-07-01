d206ed02fff3d41107f91db3a38c3193090b4771
Ira Miller <public@iramiller.com>
2018-07-01 17:06:40 -0500

    serial

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const spawn = require('guld-spawn')
 const { getJS } = require('guld-env')
+const path = require('path')
 
 async function foreach (p, cmd, args) {
   if (getJS().startsWith('node')) {
-    var found = await spawn('find', '', [p, '-type', 'f', ...args])
-    return Promise.all(found.split('\n').map(async f => {
+    var found = await spawn('find', '', [path.resolve(p), '-type', 'f', ...args], true)
+    found.split('\n').forEach(async f => {
       if (f === '') return
       return cmd(f)
-    }))
+    })
   } else throw new Error('This function only available in node for now.')
 }
 
