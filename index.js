fc1fa4a94634262264d2a33eaca216eb981d51c5
Ira Miller <public@iramiller.com>
2018-07-12 04:13:07 -0500

    guld

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { getSpawn } = require('guld-spawn')
 const { getJS } = require('guld-env')
 const path = require('path')
+const spawn = getSpawn()
 
 async function foreach (p, cmd, args) {
   if (getJS().startsWith('node')) {
-    var found = await getSpawn()('find', '', [path.resolve(p), '-type', 'f', ...args], true)
+    var found = await spawn('find', '', [path.resolve(p), '-type', 'f', ...args], true)
     found.split('\n').forEach(async f => {
       if (f === '') return
       return cmd(f)
